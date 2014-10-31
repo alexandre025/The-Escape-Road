fd2e5430735127bac9f3762eebb38c354de7cfd5
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2014-10-31 17:36:17 +0100

    Node server initialization + connection desktop

---
+++ server/server.js
@@ -0,0 +1,41 @@
+'use.strict';
+
+var server = {
+
+	init : function(){
+
+		this.io = require('socket.io').listen(3000);
+		console.log('Server listen on *: 3000');
+		this.io.on('connection',this.listen);
+
+		this.roomList = new Array();
+
+	},
+
+	listen : function(socket){
+
+		socket.on('desktop connection',function(){
+			var roomID = 0000, 
+				unique = true;
+
+			do{ // Generate a unique roomID
+				roomID = Math.round(Math.random()*1000); 
+				for (var i = 0; i < server.roomList.length; i++){
+					if(server.roomList[i] == roomID){ 
+						unique = false;
+					} 
+				};
+			}while(unique);
+			server.roomList.push(roomID);
+			socket.join(roomID);
+			console.log(server.roomList);
+			socket.emit('room join',roomID);
+		});
+
+		socket.on('desktop event',function(data){
+
+		});
+
+	}
+}
+server.init();
