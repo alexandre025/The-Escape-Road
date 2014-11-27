778ba179d2dd6469ffac3d259959a982b285cb00
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2014-11-27 01:08:43 +0100

    Commentaires

--- mobile/js/model.js
+++ mobile/js/model.js
@@ -2,28 +2,28 @@
 
 var model = {
 
-	connect : function(callback){
+	connect : function(callback){ // Connection à socket.io
 		this.socket = io.connect('http://alexandre-ferraille.local:3000');
 	},	
-	IDSubmit : function(callback){
+	IDSubmit : function(callback){ // Envoi de l'ID saisi
 		var roomID = document.getElementById('roomID').value;
 		model.socket.emit('mobile connection',roomID);
 
-		model.socket.on('wrong roomID',function(){
+		model.socket.on('wrong roomID',function(){ // Retour négatif : erreur d'ID
 			callback.call(this,false);
 		});
-		model.socket.on('room join',function(){
+		model.socket.on('room join',function(){ // Retour positif : le téléphone join la room 
 			callback.call(this,true);
 		});
 
 	},
 	newEvent : function(callback){
-		model.socket.on('desktop event',function(data){
+		model.socket.on('desktop event',function(data){ // Un évènement arrive dans la room
 			callback.call(this,data);
 		});
 	},
 
-	maxLengthCheck : function(object) {
+	maxLengthCheck : function(object) { // On s'assure que l'user entre uniquement 4 chiffres 
 		if (object.value.length > object.maxLength) {
 			object.value = object.value.slice(0, object.maxLength);
 		}
